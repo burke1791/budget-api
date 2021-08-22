10373c1381a9c0ac4b0d8b9eb7d595dabecd69cc
Chris Burke <burke1791@gmail.com>
2021-08-21 20:12:03 -0500

    adding user secrets in production because it's easy

--- BudgetApi/Program.cs
+++ BudgetApi/Program.cs
@@ -10,6 +10,14 @@ using Microsoft.Extensions.Logging;
 namespace BudgetApi {
   public class Program {
     public static void Main(string[] args) {
+      var host = new HostBuilder()
+        .ConfigureAppConfiguration((hostContext, builder) => {
+          if (hostContext.HostingEnvironment.IsDevelopment() || hostContext.HostingEnvironment.IsProduction()) {
+            builder.AddUserSecrets<Program>();
+          }
+        })
+        .Build();
+      
       CreateHostBuilder(args).Build().Run();
     }
 
